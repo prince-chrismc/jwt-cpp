6c35ebf95f86dabc670957913ece9297123eb936
Chris Mc <prince.chrismc@gmail.com>
2022-09-23 17:51:16 -0700

    Update boost-json.cpp

--- example/traits/boost-json.cpp
+++ example/traits/boost-json.cpp
@@ -47,7 +47,7 @@ int main() {
 
 
 const auto buggy = jwt::decode<traits>("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c");
-
+std::cout << "buggy alg: " << buggy.get_algorithm() << std::endl;
 
 
 
